989ce65933e3777d5745696da7a2ee0e1cf0269f
ReyDho05 <rzulnisfian>
2024-09-25 09:25:49 +0700

    first commit

---
+++ Siakad06.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Siakad06 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String nama, nim;
+        char kelas;
+        byte absen;
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        System.out.print("Masukkan Nama : ");
+        nama = sc.nextLine();
+        System.out.print("Masukkan NIM :");
+        nim = sc.nextLine();
+        System.out.print("Masukkan Kelas : ");
+        kelas = sc.nextLine().charAt(0);
+        System.out.print("Masukkan Nomor Absen : ");
+        absen = sc.nextByte();
+        System.out.print("Masukkan Nilai Kuis : ");
+        nilaiKuis = sc.nextDouble();
+        System.out.print("Masukkan Nilai Tugas : ");
+        nilaiTugas = sc.nextDouble();
+        System.out.print("Masukkan Nilai Ujian : ");
+        nilaiUjian = sc.nextDouble();
+        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian)/3;
+        System.out.print("Mahasiswa dengan nama " + nama + " (NIM "+ nim + ") ");
+        System.out.println(" Kelas  " + kelas + " Nomor Absen  "+absen );
+        System.out.print("Nilai Akhir : " + nilaiAkhir);
+
+
+    }
+}
